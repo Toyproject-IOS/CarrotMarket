babb4f9a59b35c69051eae6a77d2a5eab13ee44b
Jangilkyu <jang19.dev@gmail.com>
2022-03-15 22:03:30 +0900

    Add: home UINavigationController

---
+++ CarrotMarket/CarrotMarket/View/CMTabBarController.swift
@@ -0,0 +1,24 @@
+//
+//  CMTabBarController.swift
+//  CarrotMarket
+//
+//  Created by 장일규 on 2022/03/14.
+//
+
+import UIKit
+
+class CMTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let firstNC = UINavigationController.init(rootViewController: CarrotViewController(bgColor: UIColor.white))
+        
+        self.viewControllers = [firstNC]
+        
+        let firstTabBarItem = UITabBarItem(title: "home", image: UIImage(named: "home"), tag: 0)
+        
+        firstNC.tabBarItem = firstTabBarItem
+    }
+    
+}
