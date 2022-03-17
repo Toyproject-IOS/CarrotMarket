21ac8773eeb88639c83e9bf6d4c11a48148d2ddd
Jangilkyu <jang19.dev@gmail.com>
2022-03-17 21:52:03 +0900

    Update: tableView

--- CarrotMarket/CarrotMarket/Controller/CarrotViewController.swift
+++ CarrotMarket/CarrotMarket/Controller/CarrotViewController.swift
@@ -5,9 +5,76 @@
 //  Created by 장일규 on 2022/03/14.
 //
 
-import Foundation
 import UIKit
 
+private let CMCellId = "CMCellId"
 class CarrotViewController: UIViewController {
+    
+    let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        
+        return tableView
+    }()
+    
+    
+    convenience init(bgColor: UIColor) {
+        self.init()
+        self.view.backgroundColor = bgColor
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    func setup() {
+        view.backgroundColor = .white
+        addViews()
+        setConstraints()
+        setConfigureTableView()
+    }
+    
+    func addViews() {
+        view.addSubview(tableView)
+    }
+    
+    func setConstraints() {
+        tableViewConstratins()
+    }
+    
+    func setConfigureTableView() {
+        tableView.register(CMCell.self, forCellReuseIdentifier: CMCellId)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableViewConstratins() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+}
+
+extension CarrotViewController: UITableViewDelegate {
+    
+}
+
+extension CarrotViewController: UITableViewDataSource {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return 3
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
     
 }
