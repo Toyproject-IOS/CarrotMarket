ae28ff825e8b93b07748fc9f3d5a2d4bc0adce34
Jangilkyu <jang19.dev@gmail.com>
2022-03-20 00:09:11 +0900

    Add: RigthNavigationBarItems

---
+++ CarrotMarket/CarrotMarket/View/Element/RightNavigationBarItem.swift
@@ -0,0 +1,59 @@
+//
+//  RightNavigationBarItem.swift
+//  CarrotMarket
+//
+//  Created by 장일규 on 2022/03/19.
+//
+
+import UIKit
+
+class RightNavigationBarItem {
+    
+    let iconAlarm: UIButton = {
+        let iconAlarmImg = UIImage(named: "iconAlarm")
+        let iconAlarm:UIButton = UIButton(
+            frame: CGRect(
+                x: 0,
+                y: 0,
+                width: iconAlarmImg!.size.width,
+                height: iconAlarmImg!.size.height
+            )
+        )
+        iconAlarm.setImage(iconAlarmImg, for: .normal)
+        return iconAlarm
+    }()
+    
+    let iconSearch: UIButton = {
+        let iconSearchImg = UIImage(named: "iconSearch")
+        let iconSearch:UIButton = UIButton(
+            frame: CGRect(
+                x: 0,
+                y: 0,
+                width: iconSearchImg!.size.width,
+                height: iconSearchImg!.size.height
+            )
+        )
+        iconSearch.setImage(iconSearchImg, for: .normal)
+        return iconSearch
+    }()
+    
+    let iconList:UIButton = {
+        let iconListImg = UIImage(named: "iconList")
+        let iconList:UIButton = UIButton(
+            frame: CGRect(
+                x: 0,
+                y: 0,
+                width: iconListImg!.size.width,
+                height: iconListImg!.size.height
+            )
+        )
+        iconList.setImage(iconListImg, for: .normal)
+        return iconList
+    }()
+    
+    init() {
+        
+        
+    }
+
+}
